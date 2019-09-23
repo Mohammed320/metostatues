a235911ff23ceb924a747416491bd05ed68f5e0b
Mohammed320 <53355065+Mohammed320@users.noreply.github.com>
2019-09-24 00:27:21 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'Abo 7med🖤',
+     details: `I'm Fine💔`,
+     state: 'لَقَدْ تَعِبْتُ مِنْ مُقَآوَمَةِ هَذَآ آلْعَآلَمِ بِمَآ يَكْفِيٓ', 
+    application_id: '625803435496833046',
+        smallImageKey: 'sad',
+        smallImageText: 'why',
+        LargeImageKey: 'happy',
+        LargeImageText: 'dont leave me', 
+        instance:false, 
+   }
+    });
+});
+
+
+
+
+client.login(process.env.METO_DA)
