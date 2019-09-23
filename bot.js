e4e41caec32b6ff5f4ba99c90649e456d604a09a
Mohammed320 <53355065+Mohammed320@users.noreply.github.com>
2019-09-24 00:34:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,7 @@ client.user.setPresence({
      details: `I'm Fine💔`,
      state: 'لَقَدْ تَعِبْتُ مِنْ مُقَآوَمَةِ هَذَآ آلْعَآلَمِ بِمَآ يَكْفِيٓ', 
     application_id: '625803435496833046',
+        url: 'https://m.twitch.tv/iimetoda/',
         smallImageKey: 'sad',
         smallImageText: 'why',
         LargeImageKey: 'happy',
