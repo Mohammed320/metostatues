a3f629447fa23e6e9c2fc16b609be9ba96422df6
Mohammed320 <53355065+Mohammed320@users.noreply.github.com>
2019-09-26 20:38:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
 client.user.setPresence({
-     status: 'dnd',
+     status: 'streaming',
   game: { 
-     type: Streaming,
+     type: 0,
      name: 'Abo 7med🖤',
      url:'https://twitch.tv/iimetoda',
      details: `I'm Fine💔`,
